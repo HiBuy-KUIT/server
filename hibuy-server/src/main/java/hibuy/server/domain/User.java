beaef49c98a9e352c2119a51d382856a8ba17940
jaeuk520 <jaeuk520@naver.com>
2024-01-16 18:37:26 +0900

    feat: User Domain 생성

---
+++ hibuy-server/src/main/java/hibuy/server/domain/User.java
@@ -0,0 +1,51 @@
+package hibuy.server.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column()
+    private String status;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private Timestamp createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    public User() {
+    }
+
+    public User(String name, String email, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.status = "ACTIVE";
+    }
+}
