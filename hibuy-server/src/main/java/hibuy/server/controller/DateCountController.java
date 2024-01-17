b19fab9bbc9223dc0344bfb8af56845ceb75aa32
jaeuk520 <jaeuk520@naver.com>
2024-01-17 14:40:31 +0900

    fix: fixed DateCount Request Base Url

--- hibuy-server/src/main/java/hibuy/server/controller/DateCountController.java
+++ hibuy-server/src/main/java/hibuy/server/controller/DateCountController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("users/{userId}/count")
+@RequestMapping("/users/{userId}/count")
 @RequiredArgsConstructor
 public class DateCountController {
 
