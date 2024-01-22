d29b663aae7a57127dd77326fae8ccc7efc64a8b
beomzun <punpon53@naver.com>
2024-01-23 01:33:15 +0900

    docs: Unused import statement

--- hibuy-server/src/main/java/hibuy/server/repository/UserProductRepository.java
+++ hibuy-server/src/main/java/hibuy/server/repository/UserProductRepository.java
@@ -1,11 +1,7 @@
 package hibuy.server.repository;
 
 import hibuy.server.domain.UserProduct;
-import hibuy.server.dto.userProduct.DailyUserProductDto;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserProductRepository extends JpaRepository<UserProduct, Long> {
-
 }
