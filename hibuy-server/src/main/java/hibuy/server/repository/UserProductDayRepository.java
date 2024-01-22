5b98497c05e57cacccf3b7381f64845ff62a796d
beomzun <punpon53@naver.com>
2024-01-23 01:06:15 +0900

    feat: UserProductDayRepository 생성

---
+++ hibuy-server/src/main/java/hibuy/server/repository/UserProductDayRepository.java
@@ -0,0 +1,14 @@
+package hibuy.server.repository;
+
+import hibuy.server.domain.UserProductDay;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserProductDayRepository extends JpaRepository<UserProductDay, Long> {
+
+    @Query("select upd.day from UserProductDay upd where upd.userProduct.id=:userProductId")
+    List<Integer> findByUpId(@Param("userProductId") Long userProductId);
+
+}
