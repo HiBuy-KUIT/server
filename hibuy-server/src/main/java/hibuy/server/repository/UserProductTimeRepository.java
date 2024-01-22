a8bf0c9464aad8722613bf0d7fadd159bcbbab05
beomzun <punpon53@naver.com>
2024-01-22 15:13:25 +0900

    feat: UserProductTime Repo 생성

---
+++ hibuy-server/src/main/java/hibuy/server/repository/UserProductTimeRepository.java
@@ -0,0 +1,8 @@
+package hibuy.server.repository;
+
+import hibuy.server.domain.UserProductTime;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserProductTimeRepository extends JpaRepository<UserProductTime, Long> {
+
+}
