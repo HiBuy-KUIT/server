7251c513dc53b2f0ed8f54df9299aa2cd64fbb92
beomzun <punpon53@naver.com>
2024-01-23 01:32:48 +0900

    feat: BoolTakeRepository 생성

---
+++ hibuy-server/src/main/java/hibuy/server/repository/BoolTakeRepository.java
@@ -0,0 +1,8 @@
+package hibuy.server.repository;
+
+import hibuy.server.domain.BoolTake;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoolTakeRepository extends JpaRepository<BoolTake, Long> {
+
+}
