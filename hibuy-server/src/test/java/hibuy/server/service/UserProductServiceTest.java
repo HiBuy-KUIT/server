5a307b0ec45dc5cf0beed4f7cdff9f328c8cd423
beomzun <punpon53@naver.com>
2024-01-23 17:22:27 +0900

    fix: UserProduct 조회 테스트

--- hibuy-server/src/test/java/hibuy/server/service/UserProductServiceTest.java
+++ hibuy-server/src/test/java/hibuy/server/service/UserProductServiceTest.java
@@ -1,17 +1,20 @@
 package hibuy.server.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import hibuy.server.domain.Product;
 import hibuy.server.domain.User;
-import hibuy.server.domain.UserProduct;
+import hibuy.server.dto.userProduct.DailyUserProductDto;
 import hibuy.server.dto.userProduct.PostUserProductRequest;
 import hibuy.server.dto.userProduct.PostUserProductResponse;
+import hibuy.server.dto.userProduct.TakeStatusDto;
+import hibuy.server.repository.BoolTakeRepository;
 import hibuy.server.repository.ProductRepository;
+import hibuy.server.repository.UserProductJpaRepository;
 import hibuy.server.repository.UserProductRepository;
 import hibuy.server.repository.UserRepository;
 import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -23,32 +26,33 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 class UserProductServiceTest {
 
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    ProductRepository productRepository;
-
-    @Autowired
-    UserProductService userProductService;
-
-    @Autowired
-    UserProductRepository userProductRepository;
+    @Autowired UserRepository userRepository;
+    @Autowired ProductRepository productRepository;
+    @Autowired UserProductService userProductService;
+    @Autowired UserProductRepository userProductRepository;
+    @Autowired UserProductJpaRepository userProductJpaRepository;
+    @Autowired BoolTakeRepository boolTakeRepository;
 
     @Test
-    public void createUserProduct() throws Exception {
+    public void createUserProduct() {
         //given
-        User user = new User("bzun", "eeee", "1111");
+        User user = new User("bzun", "email_bzun", "1111");
         userRepository.save(user);
-        Product product = new Product("company", "product", 100, "url", "url2", "cat");
+
+        User userX = new User("bzun2", "email_bzun", "1111");
+        userRepository.save(user);
+
+        Product product = new Product("company", "product", 30000, "imageUrl", "productUrl", "lactofit");
         productRepository.save(product);
 
         List<Time> timeList = new ArrayList<>();
         timeList.add(Time.valueOf("09:30:00"));
         timeList.add(Time.valueOf("18:45:00"));
-        PostUserProductRequest request = new PostUserProductRequest(2, 100, timeList, 1111111, 1,
+        List<Integer> dayList = new ArrayList<>();
+        dayList.add(2);
+        dayList.add(4);
+        PostUserProductRequest request = new PostUserProductRequest(2, 100, timeList, dayList, 1,
                 user.getUserId(), product.getProductId());
-
 
         //when
         PostUserProductResponse userProduct = userProductService.createUserProduct(request);
@@ -56,5 +60,41 @@ class UserProductServiceTest {
         //then
         assertThat(userProductRepository.findById(userProduct.getUserProductId()).get()
                 .getUser()).isEqualTo(user);
+    }
+
+    @Test
+    public void findUserProduct() {
+        //given
+        User user = new User("bzun", "email_bzun", "1111");
+        User savedUser = userRepository.save(user);
+        Product product = new Product("company", "product", 30000, "imageUrl", "productUrl", "");
+        Product savedProduct = productRepository.save(product);
+
+        List<Time> timeList = new ArrayList<>();
+        timeList.add(Time.valueOf("09:30:00"));
+        timeList.add(Time.valueOf("18:45:00"));
+        List<Integer> dayList = new ArrayList<>();
+        dayList.add(2);
+        dayList.add(4);
+        PostUserProductRequest request = new PostUserProductRequest(2, 100, timeList, dayList, 1,
+                savedUser.getUserId(), savedProduct.getProductId());
+
+        PostUserProductResponse userProduct = userProductService.createUserProduct(request);
+
+        List<DailyUserProductDto> test = new ArrayList<>();
+        test.add(new DailyUserProductDto(userProduct.getUserProductId(), product.getProductName(), request.getOneTakeAmount()));
+        test.get(0).getTakeStatusDtoList().add(new TakeStatusDto(Time.valueOf("09:30:00"), "INACTIVE"));
+
+        //then
+        assertThat(userProductJpaRepository.findByUserAndDate(user.getUserId(),
+                Timestamp.valueOf("2024-01-23 09:30:00")).get(0).getProductName())
+                .isEqualTo(test.get(0).getProductName());
+        assertThat(userProductJpaRepository.findByUserAndDate(user.getUserId(),
+                Timestamp.valueOf("2024-01-23 09:30:00")).get(0).getOneTakeAmount())
+                .isEqualTo(test.get(0).getOneTakeAmount());
+        assertThat(userProductJpaRepository.findByUserAndDate(user.getUserId(),
+                Timestamp.valueOf("2024-01-23 09:30:00")).get(0).getTakeStatusDtoList().get(0).getStatus())
+                .isEqualTo(test.get(0).getTakeStatusDtoList().get(0).getStatus());
+
     }
 }
