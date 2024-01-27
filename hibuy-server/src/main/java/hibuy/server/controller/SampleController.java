d64ae9dc3c7d9d55708104aa36e11e444b7a22a7
jaeuk520 <jaeuk520@naver.com>
2024-01-27 17:33:33 +0900

    feat: 샘플 리스트 조회

--- hibuy-server/src/main/java/hibuy/server/controller/SampleController.java
+++ hibuy-server/src/main/java/hibuy/server/controller/SampleController.java
@@ -1,11 +1,14 @@
 package hibuy.server.controller;
 
 import hibuy.server.common.response.BaseResponse;
+import hibuy.server.dto.sample.GetSampleResponse;
 import hibuy.server.dto.sample.PostSampleRequest;
 import hibuy.server.service.SampleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -24,6 +27,14 @@ public class SampleController {
 
         return new BaseResponse<>(null);
 
+    }
+
+    @GetMapping()
+    public BaseResponse<List<GetSampleResponse>> getSamples() {
+
+        log.debug("[SampleController.getSamples]");
+
+        return new BaseResponse<>(sampleService.getSamples());
     }
 
 }
--- hibuy-server/src/main/java/hibuy/server/service/SampleService.java
+++ hibuy-server/src/main/java/hibuy/server/service/SampleService.java
@@ -1,11 +1,14 @@
 package hibuy.server.service;
 
 import hibuy.server.domain.Sample;
+import hibuy.server.dto.sample.GetSampleResponse;
 import hibuy.server.dto.sample.PostSampleRequest;
 import hibuy.server.repository.SampleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -24,6 +27,20 @@ public class SampleService {
                 postSampleRequest.getProductUrl());
 
         sampleRepository.save(sample);
+
+    }
+
+    public List<GetSampleResponse> getSamples() {
+
+        log.debug("[SampleService.addSample]");
+
+        return sampleRepository.findAll().stream()
+                .map(sample -> new GetSampleResponse(
+                        sample.getProductName(),
+                        sample.getCompanyName(),
+                        sample.getImageUrl(),
+                        sample.getProductUrl()
+                )).toList();
 
     }
 }
