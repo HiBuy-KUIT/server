4c36a7d4884b7f1d1f5f605ba20aa5f2b2862f70
beomzun <punpon53@naver.com>
2024-01-27 18:05:13 +0900

    feat: UserProduct update API 구현

--- hibuy-server/src/main/java/hibuy/server/controller/UserProductController.java
+++ hibuy-server/src/main/java/hibuy/server/controller/UserProductController.java
@@ -7,6 +7,7 @@ import hibuy.server.dto.userProduct.GetHomeUserProductsResponse;
 import hibuy.server.dto.userProduct.PostUserProductRequest;
 import hibuy.server.dto.userProduct.PostUserProductResponse;
 import hibuy.server.dto.userProduct.PutUserProductRequest;
+import hibuy.server.dto.userProduct.PutUserProductResponse;
 import hibuy.server.service.UserProductService;
 import java.sql.Timestamp;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -53,13 +55,13 @@ public class UserProductController {
         return new BaseResponse<>(userProductService.createUserProduct(postUserProductRequest));
     }
 
-//    @PutMapping("")
-//    public BaseResponse<PutUserProductResponse> updateUserProduct(
-//            @PathVariable Long userProductId) {
-//        log.debug("[UserProductController.updateUserProduct]");
-//
-//
-//    }
+    @PutMapping("/{userProductId}")
+    public BaseResponse<PutUserProductResponse> updateUserProduct(
+            @RequestBody PutUserProductRequest request) {
+        log.debug("[UserProductController.updateUserProduct]");
+
+        return new BaseResponse<>(userProductService.updateUserProduct(request));
+    }
 
     @DeleteMapping("/{userProductId}")
     public BaseResponse<DeleteUserProductResponse> deleteUserProduct(
