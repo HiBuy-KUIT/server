2c6e798ce13e5c456655510369ab51672a322e92
jaeuk520 <jaeuk520@naver.com>
2024-01-29 01:01:53 +0900

    style: changed JSON_PARSING_ERROR to INVALID_ERROR

--- hibuy-server/src/main/java/hibuy/server/common/response/status/BaseExceptionResponseStatus.java
+++ hibuy-server/src/main/java/hibuy/server/common/response/status/BaseExceptionResponseStatus.java
@@ -17,8 +17,8 @@ public enum BaseExceptionResponseStatus implements ResponseStatus {
     BAD_REQUEST(2000, HttpStatus.BAD_REQUEST.value(), "유효하지 않은 요청입니다."),
     URL_NOT_FOUND(2001, HttpStatus.BAD_REQUEST.value(), "유효하지 않은 URL 입니다."),
     METHOD_NOT_ALLOWED(2002, HttpStatus.METHOD_NOT_ALLOWED.value(), "해당 URL에서는 지원하지 않는 HTTP Method 입니다."),
-    METHOD_ARGUMENT_NOT_VALID(2003, HttpStatus.BAD_REQUEST.value(), "유효하지 않은 요청 값 입니다."),
-    JSON_PARSING_ERROR(2004, HttpStatus.BAD_REQUEST.value(), "JSON 파싱 에러."),
+    METHOD_ARGUMENT_NOT_VALID(2003, HttpStatus.BAD_REQUEST.value(), "유효하지 않은 입력입니다."),
+    INVALID_JSON(2004, HttpStatus.BAD_REQUEST.value(), "JSON 형식이 잘못되었습니다."),
 
     /**
      * 3000: Server, Database 오류 (INTERNAL_SERVER_ERROR)
