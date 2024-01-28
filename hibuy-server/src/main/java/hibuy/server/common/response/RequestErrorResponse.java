9f414044ef5d1799a2acfa32ca8dfe247af8783b
jaeuk520 <jaeuk520@naver.com>
2024-01-29 01:04:17 +0900

    refactor: 클라이언트 요청 입력 오류를 위한 ErrorResponse 형식 분리

---
+++ hibuy-server/src/main/java/hibuy/server/common/response/RequestErrorResponse.java
@@ -0,0 +1,30 @@
+package hibuy.server.common.response;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import hibuy.server.common.exception.FieldException;
+import hibuy.server.common.response.status.ResponseStatus;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@JsonPropertyOrder({"code", "status", "message", "timestamp"})
+public class RequestErrorResponse implements ResponseStatus {
+
+    private final int code;
+    private final int status;
+    private final String message;
+    private final LocalDateTime timestamp;
+    private final List<FieldException> exceptions;
+
+    public RequestErrorResponse(ResponseStatus status, List<FieldException> exceptions) {
+        this.code = status.getCode();
+        this.status = status.getStatus();
+        this.message = status.getMessage();
+        this.timestamp = LocalDateTime.now();
+        this.exceptions = (exceptions == null) ? new ArrayList<>() : exceptions;
+    }
+
+}
