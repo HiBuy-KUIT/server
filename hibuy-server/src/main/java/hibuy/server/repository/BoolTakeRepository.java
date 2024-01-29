b47a4f0b866981506a3fd8ef6fa50901b16e3abb
beomzun <punpon53@naver.com>
2024-01-29 15:27:11 +0900

    feat: BoolTake 조회 쿼리 구현

--- hibuy-server/src/main/java/hibuy/server/repository/BoolTakeRepository.java
+++ hibuy-server/src/main/java/hibuy/server/repository/BoolTakeRepository.java
@@ -1,6 +1,7 @@
 package hibuy.server.repository;
 
 import hibuy.server.domain.BoolTake;
+import java.sql.Timestamp;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +14,8 @@ public interface BoolTakeRepository extends JpaRepository<BoolTake, Long> {
 
     @Query("delete from BoolTake b where b.userProduct.id = :userProductId")
     void deleteByUserProductId(@Param("userProductId") Long userProductId);
+
+    @Query("select bt from BoolTake bt where bt.userProduct.id=:userProductId and bt.takeDateTime=:takeDateTime")
+    BoolTake findByUserProductAndTakeDateTime(@Param("userProductId") Long userProductId,
+            @Param("takeDateTime") Timestamp takeDateTime);
 }
