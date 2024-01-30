7382c054f2fb2700cf61cd4f8febb709d4f554f3
beomzun <punpon53@naver.com>
2024-01-30 11:39:18 +0900

    Test: UserProduct Update 테스트 추가

--- hibuy-server/src/test/java/hibuy/server/service/UserProductServiceTest.java
+++ hibuy-server/src/test/java/hibuy/server/service/UserProductServiceTest.java
@@ -8,6 +8,7 @@ import hibuy.server.dto.userProduct.DailyUserProductDto;
 import hibuy.server.dto.userProduct.GetHomeUserProductsRequest;
 import hibuy.server.dto.userProduct.PostUserProductRequest;
 import hibuy.server.dto.userProduct.PostUserProductResponse;
+import hibuy.server.dto.userProduct.PutUserProductRequest;
 import hibuy.server.dto.userProduct.TakeStatusDto;
 import hibuy.server.repository.BoolTakeRepository;
 import hibuy.server.repository.ProductRepository;
@@ -18,6 +19,7 @@ import java.sql.Time;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -34,27 +36,33 @@ class UserProductServiceTest {
     @Autowired UserProductJpaRepository userProductJpaRepository;
     @Autowired BoolTakeRepository boolTakeRepository;
 
-    @Test
-    public void createUserProduct() {
-        //given
-        User user = new User("bzun", "email_bzun", "1111");
+    private User user;
+    private Product product;
+    private List<Time> timeList;
+    private List<Integer> dayList;
+    private PostUserProductRequest request;
+
+    @BeforeEach
+    public void setUp() {
+        user = new User("bzun", "email_bzun", "1111");
         userRepository.save(user);
 
-        User userX = new User("bzun2", "email_bzun", "1111");
-        userRepository.save(user);
-
-        Product product = new Product("company", "product", 30000, "imageUrl", "productUrl", "lactofit");
+        product = new Product("company", "product", 30000, "imageUrl", "productUrl", "lactofit");
         productRepository.save(product);
 
-        List<Time> timeList = new ArrayList<>();
+        timeList = new ArrayList<>();
         timeList.add(Time.valueOf("09:30:00"));
         timeList.add(Time.valueOf("18:45:00"));
-        List<Integer> dayList = new ArrayList<>();
+
+        dayList = new ArrayList<>();
         dayList.add(2);
         dayList.add(4);
-        PostUserProductRequest request = new PostUserProductRequest(2, 100, timeList, dayList, 1,
-                user.getUserId(), product.getProductId());
 
+        request = new PostUserProductRequest(2, 100, timeList, dayList, 1,
+                user.getUserId(), product.getProductId());
+    }
+    @Test
+    public void createUserProduct() {
         //when
         PostUserProductResponse userProduct = userProductService.createUserProduct(request);
 
@@ -66,20 +74,6 @@ class UserProductServiceTest {
     @Test
     public void findUserProduct() {
         //given
-        User user = new User("bzun", "email_bzun", "1111");
-        User savedUser = userRepository.save(user);
-        Product product = new Product("company", "product", 30000, "imageUrl", "productUrl", "");
-        Product savedProduct = productRepository.save(product);
-
-        List<Time> timeList = new ArrayList<>();
-        timeList.add(Time.valueOf("09:30:00"));
-        timeList.add(Time.valueOf("18:45:00"));
-        List<Integer> dayList = new ArrayList<>();
-        dayList.add(2);
-        dayList.add(4);
-        PostUserProductRequest request = new PostUserProductRequest(2, 100, timeList, dayList, 1,
-                savedUser.getUserId(), savedProduct.getProductId());
-
         PostUserProductResponse userProduct = userProductService.createUserProduct(request);
 
         List<DailyUserProductDto> test = new ArrayList<>();
@@ -94,6 +88,47 @@ class UserProductServiceTest {
                 .getUserProductDtoList().get(0).getOneTakeAmount())
                 .isEqualTo(test.get(0).getOneTakeAmount());
         assertThat(userProductService.getHomeUserProducts(new GetHomeUserProductsRequest(Timestamp.valueOf("2024-01-23 09:30:00"),user.getUserId()))
+                .getUserProductDtoList().get(0).getTakeStatusDtoList().get(0).getStatus())
+                .isEqualTo("INACTIVE");
+    }
+
+    @Test
+    public void updateUserProduct() {
+        //given
+        PostUserProductResponse userProduct = userProductService.createUserProduct(request);
+
+        List<DailyUserProductDto> test = new ArrayList<>();
+        test.add(new DailyUserProductDto(userProduct.getUserProductId(), product.getProductName(), request.getOneTakeAmount()));
+        test.get(0).getTakeStatusDtoList().add(new TakeStatusDto(Time.valueOf("12:30:00"), "INACTIVE"));
+        test.get(0).getTakeStatusDtoList().add(new TakeStatusDto(Time.valueOf("21:30:00"), "INACTIVE"));
+
+        //수정 전
+        assertThat(userProductService.getHomeUserProducts(new GetHomeUserProductsRequest(Timestamp.valueOf("2024-01-23 09:30:00"),user.getUserId()))
+                .getUserProductDtoList().get(0).getOneTakeAmount())
+                .isEqualTo(test.get(0).getOneTakeAmount());
+
+        timeList = new ArrayList<>();
+        timeList.add(Time.valueOf("12:30:00"));
+        timeList.add(Time.valueOf("21:30:00"));
+
+        dayList = new ArrayList<>();
+        dayList.add(3);
+        dayList.add(5);
+
+        //when
+        PutUserProductRequest putUserProductRequest = new PutUserProductRequest(
+                userProduct.getUserProductId(), 1, 150, timeList, dayList, 1, user.getUserId(),
+                product.getProductId());
+        userProductService.updateUserProduct(putUserProductRequest);
+
+        //then
+        assertThat(userProductService.getHomeUserProducts(new GetHomeUserProductsRequest(Timestamp.valueOf("2024-01-31 12:30:00"),user.getUserId()))
+                .getUserProductDtoList().get(0).getProductName())
+                .isEqualTo(test.get(0).getProductName());
+        assertThat(userProductService.getHomeUserProducts(new GetHomeUserProductsRequest(Timestamp.valueOf("2024-01-31 21:30:00"),user.getUserId()))
+                .getUserProductDtoList().get(0).getOneTakeAmount())
+                .isEqualTo(1);
+        assertThat(userProductService.getHomeUserProducts(new GetHomeUserProductsRequest(Timestamp.valueOf("2024-02-02 12:30:00"),user.getUserId()))
                 .getUserProductDtoList().get(0).getTakeStatusDtoList().get(0).getStatus())
                 .isEqualTo("INACTIVE");
     }
