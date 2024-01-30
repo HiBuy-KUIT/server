ad8940359c90043b78380b0d16a4f8229dbba50b
beomzun <punpon53@naver.com>
2024-01-30 11:38:52 +0900

    fix: @Param, @Modifying 어노테이션 추가

--- hibuy-server/src/main/java/hibuy/server/repository/UserProductTimeRepository.java
+++ hibuy-server/src/main/java/hibuy/server/repository/UserProductTimeRepository.java
@@ -4,6 +4,7 @@ import hibuy.server.domain.UserProductTime;
 import java.sql.Time;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,8 +13,9 @@ public interface UserProductTimeRepository extends JpaRepository<UserProductTime
     @Query("select upt from UserProductTime upt where upt.userProduct.id in :userProductIds")
     List<UserProductTime> findByUserProductIds(@Param("userProductIds") List<Long> userProductIds);
 
+    @Modifying
     @Query("delete from UserProductTime upt where upt.userProduct.id = :userProductId")
-    void deleteByUserProductId(Long userProductId);
+    void deleteByUserProductId(@Param("userProductId") Long userProductId);
 
     @Query("select upt.takeTime from UserProductTime upt where upt.userProduct.id=:userProductId")
     List<Time> findByUpId(@Param("userProductId") Long userProductId);
