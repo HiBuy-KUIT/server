05c6f877bf93e1a8f77088d9ed60f5c26cdde843
lisiant <wkdxod321@gmail.com>
2024-01-31 17:08:52 +0900

    add: Add Annotation

--- hibuy-server/src/main/java/hibuy/server/dto/address/GetAddressListResponse.java
+++ hibuy-server/src/main/java/hibuy/server/dto/address/GetAddressListResponse.java
@@ -1,7 +1,12 @@
 package hibuy.server.dto.address;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
 public class GetAddressListResponse {
     List<AddressDTO> addresses;
 }
