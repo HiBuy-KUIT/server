fbe6d98acdd422922562f6f4273e3e1ab7e3bf12
jaeuk520 <jaeuk520@naver.com>
2024-02-07 12:22:23 +0900

    fix: @NotBlank to @NotNull

--- hibuy-server/src/main/java/hibuy/server/dto/userProduct/PostUserProductRequest.java
+++ hibuy-server/src/main/java/hibuy/server/dto/userProduct/PostUserProductRequest.java
@@ -1,6 +1,7 @@
 package hibuy.server.dto.userProduct;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.sql.Time;
 import java.util.List;
@@ -18,19 +19,19 @@ public class PostUserProductRequest {
 
     //1회 섭취량
     @Positive
-    @NotBlank(message = "1회 섭취량을 입력해주세요.")
+    @NotNull(message = "1회 섭취량을 입력해주세요.")
     private int oneTakeAmount;
 
     //총 제품량
     @Positive
-    @NotBlank(message = "드실 제품의 총량을 입력해주세요.")
+    @NotNull(message = "드실 제품의 총량을 입력해주세요.")
     private int totalAmount;
 
     //섭취 시간
-    @NotBlank(message = "제품을 섭취하실 시간을 선택해주세요.")
+    @NotNull(message = "제품을 섭취하실 시간을 선택해주세요.")
     private List<Time> takeTimeList;
 
-    @NotBlank(message = "제품을 섭취하실 요일을 선택해주세요.")
+    @NotNull(message = "제품을 섭취하실 요일을 선택해주세요.")
     private List<Integer> takeDay;
 
     //알림 여부
