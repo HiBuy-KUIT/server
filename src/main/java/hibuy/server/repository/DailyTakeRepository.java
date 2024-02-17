8e3bc26074899745687d576a82caddbe47146e5d
jaeuk520 <jaeuk520@naver.com>
2024-02-18 02:32:29 +0900

    feat: findTakeDatesByUserIdAAndTakeDate 쿼리 추가

--- src/main/java/hibuy/server/repository/DailyTakeRepository.java
+++ src/main/java/hibuy/server/repository/DailyTakeRepository.java
@@ -1,15 +1,22 @@
 package hibuy.server.repository;
 
 import hibuy.server.domain.DailyTake;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.swing.text.html.Option;
 import java.sql.Date;
 import java.util.List;
+import java.util.Optional;
 
 public interface DailyTakeRepository extends JpaRepository<DailyTake, Long> {
 
-    @Query("SELECT dt.takeDate FROM DailyTake dt JOIN dt.user u WHERE u.userId = :user_id AND u.status = 'ACTIVE' AND dt.status = 'ACTIVE'")
-    List<Date> findTakeDatesByUserId(@Param("user_id") Long userId);
+    @Query("SELECT dt.takeDate FROM DailyTake dt JOIN dt.user u WHERE u.userId = :userId AND u.status = 'ACTIVE' AND dt.status = 'ACTIVE'")
+    List<Date> findTakeDatesByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT dt FROM DailyTake dt JOIN dt.user u " +
+            "WHERE u.userId = :userId AND u.status = 'ACTIVE' AND dt.takeDate = :takeDate")
+    Optional<DailyTake> findTakeDatesByUserIdAAndTakeDate(@Param("userId") Long userId, @Param("takeDate") Date takeDate);
 }
