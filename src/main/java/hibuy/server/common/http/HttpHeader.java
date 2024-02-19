2db31b1da8c6e6a1bc0f1dbc7c1075915faec07c
jaeuk520 <jaeuk520@naver.com>
2024-02-19 15:51:38 +0900

    feat: HttpHeader enum 클래스 추가

---
+++ src/main/java/hibuy/server/common/http/HttpHeader.java
@@ -0,0 +1,16 @@
+package hibuy.server.common.http;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum HttpHeader {
+
+    CONTENT_TYPE_URL_ENCODED("Content-type", "application/x-www-form-urlencoded;charset=utf-8"),
+    AUTHORIZATION("Authorization", "Bearer ");
+
+    private final String headerName;
+    private final String headerValue;
+
+}
