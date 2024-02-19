b330c4bb34bb65b20214efc6e9e995f39c65fba8
jaeuk520 <jaeuk520@naver.com>
2024-02-19 15:28:07 +0900

    refactor: DateCount update 쿼리 삭제

--- src/main/java/hibuy/server/repository/DateCountRepository.java
+++ src/main/java/hibuy/server/repository/DateCountRepository.java
@@ -13,7 +13,4 @@ public interface DateCountRepository extends JpaRepository<DateCount, Long> {
     @Query("SELECT dc FROM DateCount dc JOIN dc.user u WHERE u.userId = :user_id AND u.status = 'ACTIVE'")
     Optional<DateCount> findDateCountByUserId(@Param("user_id") Long userId);
 
-    @Modifying(clearAutomatically = true)
-    @Query("UPDATE DateCount dc set dc.dateCount = dc.dateCount+1 WHERE dc.user.userId = :user_id AND dc.user.status = 'ACTIVE'")
-    int updateDateCountByUserId(@Param("user_id") Long userId);
 }
