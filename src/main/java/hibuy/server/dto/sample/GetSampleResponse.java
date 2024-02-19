72682cfa216c874b449463d3211ed5cb22b6fa48
jaeuk520 <jaeuk520@naver.com>
2024-02-19 16:27:59 +0900

    fix: GetSampleResponse 버그 해결

--- src/main/java/hibuy/server/dto/sample/GetSampleResponse.java
+++ src/main/java/hibuy/server/dto/sample/GetSampleResponse.java
@@ -2,7 +2,9 @@ package hibuy.server.dto.sample;
 
 import hibuy.server.domain.Sample;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class GetSampleResponse {
 
