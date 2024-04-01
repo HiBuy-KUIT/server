8621e0de3c670fda9eeecbb322fefa46dd372302
jaeuk520 <jaeuk520@naver.com>
2024-04-01 15:01:15 +0900

    refactor: GetDateCountResponse 정적 팩토리 메서드 적용

--- src/main/java/hibuy/server/dto/dateCount/GetDateCountResponse.java
+++ src/main/java/hibuy/server/dto/dateCount/GetDateCountResponse.java
@@ -1,11 +1,17 @@
 package hibuy.server.dto.dateCount;
 
+import hibuy.server.domain.DateCount;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class GetDateCountResponse {
     private Long userId;
     private int dateCount;
+
+    public static GetDateCountResponse of(DateCount dateCount) {
+        return new GetDateCountResponse(dateCount.getUser().getUserId(), dateCount.getDateCount());
+    }
 }
